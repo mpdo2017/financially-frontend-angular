c7bf9eba27c6ab32c0ecb28689159a32c31a5d8c
mpdo2017@gmail.com <shellyb30>
2020-04-28 16:38:10 -0400

    Added dashboard and shared component files and styling.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SharedComponent } from './shared/shared.component';
+import { AppHeaderComponent } from './shared/app-header/app-header/app-header.component';
+import { AppFooterComponent } from './shared/app-footer/app-footer/app-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    SharedComponent,
+    AppHeaderComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule
